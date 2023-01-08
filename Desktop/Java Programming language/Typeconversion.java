4ad0705c72168d6176af8a7121aa40f0cbf8e3ac
MohiniChanpuriya <chanpuriyamohini51@gmail.com>
2023-01-08 22:56:21 +0530

    conversion

---
+++ Desktop/Java Programming language/Typeconversion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Typeconversion {
+    public static void main(String args []) {
+        Scanner sc = new Scanner(System.in);
+
+        //int number = sc.nextFloat();
+        float number = sc.nextInt();
+        System.out.println(number);
+
+        // int num = sc.nextDouble();
+        double num = sc.nextInt();
+        System.out.println(num);
+    }
+}
