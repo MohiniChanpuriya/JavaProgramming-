acee2996aa7c645938ac379f1b9fb211f9b29e13
MohiniChanpuriya <chanpuriyamohini51@gmail.com>
2023-01-08 16:08:12 +0530

    initial commit

---
+++ Desktop/Java Programming language/Secondcode.java
@@ -0,0 +1,14 @@
+public class Secondcode {
+    public static void main(String args []) {
+        /* Printing pattern 
+           * * * *
+           * * *
+           * *
+           *
+        */ 
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
