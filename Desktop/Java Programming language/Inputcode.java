0e36fec76e594c1aeecd87c78b2add8f18bb4118
MohiniChanpuriya <chanpuriyamohini51@gmail.com>
2023-01-08 16:24:31 +0530

    inputcode

---
+++ Desktop/Java Programming language/Inputcode.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Inputcode {
+    public static void main(String args []) {
+        Scanner str = new Scanner(System.in);
+        String Fname= str.nextLine();
+        String Sname= str.nextLine();
+        String conc = Fname+" "+Sname;
+        System.out.println(conc);
+
+    }
+    
+}
