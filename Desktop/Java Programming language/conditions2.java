00f381283777c44c9983fbd49e79502cd3469220
MohiniChanpuriya <chanpuriyamohini51@gmail.com>
2023-01-11 16:29:30 +0530

    Odd Even

---
+++ Desktop/Java Programming language/conditions2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class conditions2 {
+    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if(x % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+
+    }
+}
