a75762f338d8115e7a9a7fe43cab7eff6c1d8810
MohiniChanpuriya <chanpuriyamohini51@gmail.com>
2023-02-17 15:43:12 +0530

    initial commit

--- Desktop/Java Programming language/practice5.java
+++ Desktop/Java Programming language/practice5.java
@@ -2,16 +2,30 @@ import java.util.*;
 public class practice5 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        
+        int number;
+        int choice;
+        int evenSum = 0;
+        int oddSum = 0;
 
-        do{
-            System.out.print("Enter your Number : ");
-            int n = sc.nextInt();
+        do {
+            System.out.print("Enter the number ");
+            number = sc.nextInt();
 
-            if(n % 10 == 0){
-                break;
+            if (number % 2 == 0) {
+                evenSum += number;
+            } else {
+                oddSum += number;
             }
-            System.out.println(n);
-        } while(true);
+
+            System.out.print("Do you want to continue? press 1 for yes or 0 for no");
+            
+            choice = sc.nextInt();
+
+        } while(choice==1);
+
+        System.out.println("Sum of even number: " + evenSum);
+        System.out.println("Sum of odd numbers: " + oddSum);
     }
     
 }
